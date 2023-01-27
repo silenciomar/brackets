c311b713014ad89acc118601a711b695082968fb
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-01-27 09:54:21 +0600

    fix: delete mistakes and change a little bit my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairsBracket = bracketsConfig.reduce((acc, obj) => {
+    acc[obj[1]] = obj[0];
+    return acc;
+  }, {});
+  let stack = [];
+  let arr = str.split('');
+
+  arr.map(curElem => {
+    let topElem = stack[stack.length - 1];
+
+    (pairsBracket[curElem] === topElem && stack.length > 0) ? stack.pop() : stack.push(curElem);
+  })
+  return stack.length === 0;
 }
